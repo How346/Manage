085f1bc25039b2a093e283264f0ffe883eb77a67
How346 <How@pindush.click>
2026-08-19 16:23:08 +0530

    Rename smoke test for School Manager widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,17 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('Material app smoke test', (tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: Scaffold(
-          body: Center(
-            child: Text('School Manager'),
+  testWidgets(
+    'School Manager basic Flutter smoke test',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: Text('School Manager'),
+            ),
           ),
         ),
-      ),
-    );
+      );
 
-    expect(find.text('School Manager'), findsOneWidget);
-  });
+      expect(
+        find.text('School Manager'),
+        findsOneWidget,
+      );
+    },
+  );
 }
